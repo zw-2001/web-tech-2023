ae6834f1cca9242d7aa11ba0730f9a32b0c3c001
zw-2001 <58522079+zw-2001@users.noreply.github.com>
2024-01-01 17:29:10 +0100

    Initialization

---
+++ src/main/java/com/miage/java/demo/FinanceConsultingApplication.java
@@ -0,0 +1,13 @@
+package com.miage.java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceConsultingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miage/java/demo/FinanceConsultingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.java.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceConsultingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
